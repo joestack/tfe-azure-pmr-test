bc93eee8016c4b3140c4cd645110e98cc8f2aaca
joestack <joern@joern-online.de>
2020-04-14 14:21:07 +0200

    5 commit

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 module "network" {
+  depends_on = [azurerm_resource_group.test]
   source  = "app.terraform.io/JoeStack/network/azurerm"
   version = "3.0.1"
   resource_group_name = azurerm_resource_group.test.name
