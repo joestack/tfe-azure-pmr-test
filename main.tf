19a4fe2c10f99a1da254a534b54e3ff37ae49338
joestack <joern@joern-online.de>
2020-04-14 14:23:37 +0200

    6 commit

--- main.tf
+++ main.tf
@@ -7,16 +7,15 @@ provider "azurerm" {
 
 
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "rs_demo" {
   name     = "my-resources"
   location = "West Europe"
 }
 
 module "network" {
-  depends_on = [azurerm_resource_group.test]
   source  = "app.terraform.io/JoeStack/network/azurerm"
   version = "3.0.1"
-  resource_group_name = azurerm_resource_group.test.name
+  resource_group_name = azurerm_resource_group.rs_demo.name
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
